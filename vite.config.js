0ba4cf2df9eb6da700f80a32dda9bd1300729b2c
Stanley B. Howard <shbackup2252@gmail.com>
2026-04-26 18:23:48 -0400

    Simplify: Remove unnecessary Rollup config to match working project

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/booksreact-aws/',
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  }
+  base: '/booksreact-aws/'
 })
 
