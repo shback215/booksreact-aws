596babefbd890793fd94b7346f5a19d23cfa3c20
Stanley B. Howard <shbackup2252@gmail.com>
2026-04-26 14:20:43 -0400

    Fix routing conflicts with BrowserRouter basename

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
       
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/booksreact-aws" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </div>
